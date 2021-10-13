17757c57e78aee08d629a820cd72e906b5fc3d9c
ad451 <71942892+ad451@users.noreply.github.com>
2021-10-13 15:30:23 +0530

    Update LinearSearch.py

--- python/search/LinearSearch.py
+++ python/search/LinearSearch.py
@@ -5,6 +5,7 @@
 # it returns -1 if the element is not found in the array.
 
 def LinearSearch(arr,key):
+    # we use a for loop to iterate through the array. 
     for i in range(len(arr)):
         if arr[i]==key:
             return i   # this returns the index if the key is found
@@ -17,6 +18,16 @@ def LinearSearch(arr,key):
 # Creating array of the size n
 # Size would be taken into the variable n from user
 def main():
+    print("please enter the size of the array :")
+    n=int(input()) # takes size as input
+    array=[]
+    print("please enter the elements of the array :")
+    for j in range(n):     # for loop for taking the elements as input
+        array.append(int(input()))
+    print("please enter the key element :")    
+    x=int(input())         # the key is taken as input
+    print(LinearSearch(array,x))    # linear search is called  and the result is printed
+
 main()
 
 
